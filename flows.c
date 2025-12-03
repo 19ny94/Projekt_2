e99347bf77d4bdeb4eecc71b3a998bdae5461109
i_use_arch_btw <zanejrode@gmail.com>
2025-12-03 23:37:21 +0100

    Практически готово

--- flows.c
+++ flows.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define IN 1 /*stav slova = pozice indexu je na slove*/
 #define OUT 0 /*stav slova = pozice indexu je mimo slova*/
@@ -28,20 +29,49 @@ typedef struct{
 	unsigned int count; /*zda se, ze pocet toku byt zaponym nemuze*/
 	/*cislo klastera*/
 	unsigned int* flowid;
-	/*obsah dat*/
-	double* total_bytes;
-	/*IP odesilatele*/
-	int src_ip;
-	/*IP prijemce*/
-	int dst_ip;
+	/*obsah dat(total_bytes)*/
+	double* total_b;
 	/*delka toku dat*/
 	double* flow_dur;
 	/*kolik paketu se poslalu/dostalo*/
-	double* packet_count;
+	double* pac_c;
 	/*prumerna delka*/
-	double* avg_int;
+	double* d_avg;
+	/*prumerna delka packetu*/
+	double* s_avg;
 	
 } data_z_souboru;
+
+typedef struct{
+	unsigned int* flows;
+	int pocet;
+
+} Flows;
+
+typedef struct{
+	Flows* data;
+	int size;
+} Clusters;
+
+
+
+Flows *flows_ctor(){
+
+	Flows *f=malloc(sizeof(Flows));
+	f->flows = NULL;
+	f->pocet = 0;
+
+	return f;
+}
+
+Clusters *clusters_ctor(){
+
+	Clusters *c=malloc(sizeof(Clusters));
+	c->data = NULL;
+	c->size = 0;
+
+	return c;
+}
 
 data_z_souboru *soubor_data_ctor(){
 
@@ -49,12 +79,11 @@ data_z_souboru *soubor_data_ctor(){
 	
 	s_data->count = 0;
 	s_data->flowid = NULL;
-	s_data->src_ip = 0;
-	s_data->dst_ip = 0;
-	s_data->total_bytes = NULL;
+	s_data->total_b = NULL;
 	s_data->flow_dur = NULL;
-	s_data->packet_count = NULL;
-	s_data->avg_int = NULL;
+	s_data->pac_c = NULL;
+	s_data->d_avg = NULL;
+	s_data->s_avg = NULL;
 
 	return s_data;
 }
@@ -62,47 +91,25 @@ data_z_souboru *soubor_data_ctor(){
 double *resize(double *arr, unsigned int new_size){
 	
 	double* new_arr = realloc(arr, new_size * sizeof(double));
-	
+
 	if(new_arr == NULL){
-	free(new_arr);
+	free(arr);
 	exit(1);
 	}
 
 	return new_arr;
 }
 
-double mocnina(int x, int y){
 
-double nove_x = x;
-
-for(int i = 0; i < y - 1; i++){
-	x*= nove_x;
-	}
-	
-	if(y == 0){
-		return 1;
-	}
-	
-	if(y < 0){
-		/*Jedine mozne reseni jak udelat zapornou mocninu
-		 * funkce se zase vola z duvodu, protoze v tom cyklu
-		 * nemuzu jako by vzat 10 -1krat*/
-		return 1/mocnina(x, -y);
-	}
-
-	return x;
-}
 
 double double_z_stringu(char *c){
 
 double result = 0;
 int len = strlen(c);
-/*jsou to dva pole, prvni ulozi cisla pred carou
- * druha ulozi po care, rozdil v tom,
- * ze cast po care budu mocnit na zapornou*/	
-char *pred_carou = malloc(len + 1);
-char *po_care = malloc(len + 1);
-int pozice_cary = 0;
+
+/*nastavim na -1 protoze index cary muze 
+ * byt jakymkoliv od 0 do nekonecna*/	
+int pozice_cary = -1;
 
 /*treba chci dostat int 10 z c[0] = 1, c[1] = 0
  *
@@ -120,34 +127,23 @@ int pozice_cary = 0;
 		}
 	}
 	/*pokud neni - provede se bezne pretypovani*/
-	if(pozice_cary == 0){
+	if(pozice_cary == -1){
 	for(int i = 0; i < len; i++){
-		result += ((c[i] - '0') * mocnina (10, len -1 -i));
+		result += ((c[i] - '0') * pow(10, len -1 -i));
 		}
 	}
-	/*do "po_care" se zapisou desetinna cisla, do "pred_carou" - cela*/	
-	if(pozice_cary != 0){
-	for(int i = pozice_cary; c[i] != '\0'; i++){
-		po_care[i - pozice_cary] = c[i + 1];
-		}
+	else{
+
+		/*bezne pretypovani*/
 	for(int i = 0; i < pozice_cary; i++){
-		pred_carou[i] = c[i];
+	result += ((c[i] - '0') * pow(10, pozice_cary - 1 -i));
 		}
 
-		pred_carou[pozice_cary] = '\0';
-		po_care[len - pozice_cary - 1] = '\0';
-int len1 = strlen(pred_carou);
-		/*v podstate to stejny ale zaporna mocnina*/
-	for(int i = 0; po_care[i] != '\0'; i++){
-		result += ((po_care[i] - '0') * mocnina(10, -1 -i));
-		}
-		/*bezne pretypovani*/
-	for(int i = 0; pred_carou[i] != '\0'; i++){
-		result += ((pred_carou[i] - '0') * mocnina(10,len1 -1 -i));
+	/*v podstate to stejny ale zaporna mocnina*/
+	for(int i = pozice_cary; i < len; i++){
+	result += ((c[i] - '0') * pow(10, -1 -i));
 		}
 	}
-	free(pred_carou);
-	free(po_care);
 	return result;
 }
 
@@ -192,7 +188,7 @@ int nacitani_nazvu_souboru(int argc, char** argv, vstupni_data *data){
 
 void nacitani_argumentu(char** argv, vstupni_data *data){
 
-double *pole_pro_kontrolu[4] = {&data->WB, &data->WD, &data->WD, &data->WS};
+double *pole_pro_kontrolu[4] = {&data->WB, &data->WT, &data->WD, &data->WS};
 	data->N = atoi(argv[2]);
 	data->WB = atoi(argv[3]);
 	data->WT = atoi(argv[4]);
@@ -204,9 +200,6 @@ double *pole_pro_kontrolu[4] = {&data->WB, &data->WD, &data->WD, &data->WS};
 		printf("Argumenty jsou realna nezaporna cisla\n");
 		exit(1);
 			}
-		else{
-		printf("%.1lf ", *pole_pro_kontrolu[i]);
-		}
 	}
 }
 
@@ -233,8 +226,6 @@ void nacitani_vstupnich_dat(vstupni_data *data, data_z_souboru *sou_data ){
 	/*pri prvni iteraci dostavam count, cili pocet vstpnich klasteru*/
 	fgets(buffer, sizeof(buffer), soubor);
 	
-	printf("%s\n", buffer);
-	
 	int len = strlen(buffer);
 
 	for(int i = 0; i < len - 1; i++){
@@ -249,12 +240,12 @@ void nacitani_vstupnich_dat(vstupni_data *data, data_z_souboru *sou_data ){
 	}
 
 	sou_data->flowid=realloc(sou_data->flowid, sizeof(int) * sou_data->count);
-	sou_data->total_bytes=resize(sou_data->total_bytes,sou_data->count);
+	sou_data->total_b=resize(sou_data->total_b,sou_data->count);
 	sou_data->flow_dur=resize(sou_data->flow_dur,sou_data->count);
-	sou_data->packet_count=resize(sou_data->packet_count, sou_data->count);
+	sou_data->pac_c=resize(sou_data->pac_c, sou_data->count);
+	sou_data->d_avg=resize(sou_data->d_avg, sou_data->count);
+	sou_data->s_avg=resize(sou_data->s_avg, sou_data->count);
 
-	sou_data->avg_int=resize(sou_data->avg_int, sou_data->count);
-	
 	char *c = malloc(sizeof(char));
 
 	int stav = OUT;
@@ -262,9 +253,7 @@ void nacitani_vstupnich_dat(vstupni_data *data, data_z_souboru *sou_data ){
 	int i = 0;
 	int g = 0;
 	int radek = 0;
-	printf("\n");/*PAK ODSTANIT*/
 	while((g = fgetc(soubor)) != EOF){
-	putchar(g);/*PAK ODSTANIT*/
 		
 		if(g == ' ' || g == '\t' || g == '\n'){
 		stav = OUT;
@@ -286,16 +275,16 @@ void nacitani_vstupnich_dat(vstupni_data *data, data_z_souboru *sou_data ){
 		}
 		
 		if(stav == IN && slovo == 4){
-nacitani_jednotlyvych_dat(&c,&i,g,radek,sou_data->total_bytes);
+nacitani_jednotlyvych_dat(&c,&i,g,radek,sou_data->total_b);
 		}
 		if(stav == IN && slovo == 5){
 nacitani_jednotlyvych_dat(&c,&i,g,radek,sou_data->flow_dur);
 		}
 		if(stav == IN && slovo == 6){
-nacitani_jednotlyvych_dat(&c,&i,g,radek,sou_data->packet_count);
+nacitani_jednotlyvych_dat(&c,&i,g,radek,sou_data->pac_c);
 		}
 		if(stav == IN && slovo == 7){
-nacitani_jednotlyvych_dat(&c,&i,g,radek,sou_data->avg_int);
+nacitani_jednotlyvych_dat(&c,&i,g,radek,sou_data->d_avg);
 		}
 
 		if(g == '\n'){
@@ -303,29 +292,8 @@ nacitani_jednotlyvych_dat(&c,&i,g,radek,sou_data->avg_int);
 		radek++;
 		}
 	}
-	/*PAK ODSTANIT*/
-	printf("\nFlowid: ");
-	for(int j = 0; j < sou_data->count; j++){
-	printf("%d ", sou_data->flowid[j]);
-	}
-	printf("\nTotal_bytes: ");
-	for(int j = 0; j < sou_data->count; j++){
-	printf("%.0f ", sou_data->total_bytes[j]);
-	}
-	printf("\nFlow_duration: ");
-	for(int j = 0; j < sou_data->count; j++){
-	printf("%.0f ", sou_data->flow_dur[j]);
-	}
-	printf("\nPacket_count: ");
-	for(int j = 0; j < sou_data->count; j++){
-	printf("%.0f ", sou_data->packet_count[j]);
-	}
-	printf("\nAvg_int: ");
-	for(int j = 0; j < sou_data->count; j++){
-	printf("%.2f ", sou_data->avg_int[j]);
-	}
+
 	free(c);
-	printf("\n");
 	fclose(soubor);
 }
 
@@ -337,18 +305,131 @@ void data_dtor(vstupni_data *data){
 
 void soubor_data_dtor(data_z_souboru *sou_data){
 free(sou_data->flowid);
-free(sou_data->total_bytes);
+free(sou_data->total_b);
 free(sou_data->flow_dur);
-free(sou_data->packet_count);
-free(sou_data->avg_int);
+free(sou_data->pac_c);
+free(sou_data->d_avg);
+free(sou_data->s_avg);
 free(sou_data);
+}
+
+int pokud_je_prvkem(data_z_souboru *s_data, unsigned potencialni_flowid){
+	
+	for(int i = 0; i < s_data->count; i++){
+		if(s_data->flowid[i] == potencialni_flowid){
+			return i;
+			}
+		}
+	return -1;
+}
+
+double dist(vstupni_data *data, data_z_souboru *s_data, int A, int B){
+
+double result = 0;
+
+	result += sqrt(data->WB * pow(s_data->total_b[A] - s_data->total_b[B], 2)+
+		       data->WT * pow(s_data->flow_dur[A] - s_data->flow_dur[B],2)+
+		       data->WD * pow(s_data->d_avg[A] - s_data->d_avg[B], 2)+
+		       data->WS * pow(s_data->s_avg[A] - s_data->s_avg[B], 2));
+	return result;
+}
+
+void inicializace_pole_clusteru(Clusters *c, data_z_souboru *s_data){
+
+	c->data = malloc(sizeof(Flows) * s_data->count);
+	c->size = s_data->count;
+	
+	for(int i = 0; i < s_data->count; i++){
+	c->data[i].flows = malloc(sizeof(unsigned int));
+	c->data[i].flows[0] = s_data->flowid[i];
+	c->data[i].pocet = 1;
+	}
+}
+
+void hledani_minima(Clusters *c, data_z_souboru *s_data, vstupni_data *data, int *idx_A, int *idx_B){
+	
+double min = 999999999;
+int d = 0;
+
+	for(int i = 0; i < c->size; i++){
+		for(int j = i + 1; j < c->size; j++){
+
+			for(int a = 0; a < c->data[i].pocet; a++){
+		int x = pokud_je_prvkem(s_data, c->data[i].flows[a]);	
+				for(int b = 0; b < c->data[j].pocet; b++){
+		int y = pokud_je_prvkem(s_data, c->data[j].flows[b]);
+
+		if(x == -1 || y == -1){
+		printf("Nevalidni data\n");
+		exit(1);	
+		}
+		
+			d = dist(data, s_data, x, y);
+
+			if(min > d){
+			min = dist(data, s_data, x, y);
+			*idx_A = i;
+			*idx_B = j;
+					}
+				}
+			}
+     		}
+	}
+}
+
+
+void shlukovaci_analyza(Clusters *c, data_z_souboru *s_data, vstupni_data *data){
+
+int idx_A = 0;
+int idx_B = 0;
+
+	while(c->size > data->N){
+	hledani_minima(c, s_data, data, &idx_A, &idx_B);
+
+	int stary_pocet = c->data[idx_A].pocet;
+	int new_pocet = c->data[idx_A].pocet + c->data[idx_B].pocet;
+	c->data[idx_A].pocet = new_pocet;	
+	c->data[idx_A].flows = realloc(c->data[idx_A].flows, sizeof(int) * new_pocet);
+
+	for(int i = 0 ; i < c->data[idx_B].pocet; i++){
+	c->data[idx_A].flows[stary_pocet + i] = c->data[idx_B].flows[i];
+	}
+
+	/*prednaska IZP 7*/
+	for(int i = idx_B; i < c->size - 1; i++){
+	c->data[i] = c->data[i + 1];
+	}
+	c->size--;
+	}
+}
+
+void print_vysledku(Clusters *c){
+
+	printf("Clusters: \n");
+	for(int i = 0; i < c->size; i++){
+	printf("cluster %d: ", i);
+		for(int j = 0; j < c->data[i].pocet; j++){
+		printf("%d ", c->data[i].flows[j]);
+		}
+		printf("\n");
+	}
+}
+
+void clusters_dtor(Clusters *c){
+
+	for(int i = 0; i < c->size; i++){
+		free(c->data[i].flows);
+		}
+	free(c->data);
+	free(c);
 }
 
 int main(int argc, char** argv){
 
 	vstupni_data *data = data_ctor();
-
 	data_z_souboru *sou_data = soubor_data_ctor();
+	Flows *f = flows_ctor();
+	Clusters *c = clusters_ctor();
 
 	nacitani_nazvu_souboru(argc,argv,data);
 	
@@ -356,9 +437,17 @@ int main(int argc, char** argv){
 
 	nacitani_vstupnich_dat(data, sou_data);
 
+	inicializace_pole_clusteru(c, sou_data);
+
+	shlukovaci_analyza(c, sou_data, data);
+
+	print_vysledku(c);
+
 	data_dtor(data);
 	soubor_data_dtor(sou_data);
+	clusters_dtor(c);
 
 	return 0; 
 }
+
 
